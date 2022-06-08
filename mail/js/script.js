df71b434dcac64dae642cf3597d09d6ef3c19391
cibs84 <d.tilesi@gmail.com>
2022-06-08 15:44:11 +0200

    Mail - comments created

---
+++ mail/js/script.js
@@ -0,0 +1,22 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// CREO UNA LISTA DI EMAIL
+
+
+// CHIEDO ALL'UTENTE DI COMUNICARE LA SUA EMAIL
+
+
+// CONTROLLO CHE L'EMAIL INSERITA SIA PRESENTE NELL'ELENCO
+    // CREO VARIABILE 'mailFound = false' PER SALVARE SE L'EMAIL E' PRESENTE NELL'ELENCO OPPURE NO
+
+    // SCORRO LA LISTA EMAIL
+
+    // Per ogni elemento: 
+    // SE L'EMAIL INSERITA DALL'UTENTE SI TROVA NELL'ELENCO ALLORA 'mailFound = true'
+
+// STAMPO UN MESSAGIO SULL'ESITO DEL CONTROLLO
+    // SE 'mailFound === true' ALLORA DICO CHE L'EMAIL SI TROVA NELL'ELENCO
+    // ALTRIMENTI DICO CHE L'EMAIL NON SI TROVA NELL'ELENCO
