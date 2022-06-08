15cdfbe057c59779abc1f1830e270ba71d82d2f9
cibs84 <d.tilesi@gmail.com>
2022-06-08 16:09:18 +0200

    Mail - completed

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,19 +4,36 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 
 // CREO UNA LISTA DI EMAIL
-
+const eMails = ['costanza@boolean.it', 'alessandro@boolean.it', 'paolo@boolean.it', 'massimo@boolean.it', 'marilena@boolean.it'];
 
 // CHIEDO ALL'UTENTE DI COMUNICARE LA SUA EMAIL
-
+const userEmail = prompt('Inserisci la tua e-mail');
 
 // CONTROLLO CHE L'EMAIL INSERITA SIA PRESENTE NELL'ELENCO
-    // CREO VARIABILE 'mailFound = false' PER SALVARE SE L'EMAIL E' PRESENTE NELL'ELENCO OPPURE NO
+    // CREO VARIABILE 'eMailFound = false' PER SALVARE SE L'EMAIL E' PRESENTE NELL'ELENCO OPPURE NO
+let eMailFound = false;
 
     // SCORRO LA LISTA EMAIL
-
+for (let i = 0; i < eMails.length; i++) {
+    const thisEmail = eMails[i];
+    
     // Per ogni elemento: 
-    // SE L'EMAIL INSERITA DALL'UTENTE SI TROVA NELL'ELENCO ALLORA 'mailFound = true'
+    // SE L'EMAIL INSERITA DALL'UTENTE SI TROVA NELL'ELENCO ALLORA 'eMailFound = true'
+    if (userEmail === thisEmail) {
+        eMailFound = true; 
+    }
+};
 
 // STAMPO UN MESSAGIO SULL'ESITO DEL CONTROLLO
-    // SE 'mailFound === true' ALLORA DICO CHE L'EMAIL SI TROVA NELL'ELENCO
+    // CREO LA VARIABILE 'msg' PER IL MESSAGGIO DA STAMPARE
+let msg;
+    // SE 'eMailFound === true' ALLORA DICO CHE L'EMAIL SI TROVA NELL'ELENCO
+if (eMailFound === true) {
+    msg = `L'email inserita è presente nell'elenco`;
     // ALTRIMENTI DICO CHE L'EMAIL NON SI TROVA NELL'ELENCO
+} else {
+    msg = `L'email inserita NON è presente nell'elenco`;
+};
+
+    // STAMPA MESSAGGIO IN HTML
+document.querySelector('h1').innerHTML = msg;
