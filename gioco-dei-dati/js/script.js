09d958918d21d0d8fce4e248a54a9a8f40dc2e1d
cibs84 <d.tilesi@gmail.com>
2022-06-08 18:01:25 +0200

    improved comments

--- gioco-dei-dati/js/script.js
+++ gioco-dei-dati/js/script.js
@@ -2,17 +2,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
 // GENERO UN NUMERO RANDOM DA 1 A 6
     // PER IL GIORCATORE
 const playerNumber = Math.floor(Math.random() * 9) + 1;
     // PER IL COMPUTER
 const pcNumber = Math.floor(Math.random() * 9) + 1;
 
+
 // CREO LA VARIABILE 'winner' PER SALVARE CHI HA VINTO
 let winner = 'nobody';
 
+
 // CONFRONTO I NUMERI DEL GIOCATORE E DEL COMPUTER PER DETERMINARE CHI HA VINTO
-// SE 'playerNumber' E' MAGGIORE DI 'pcNumber' ALLORA "winner = 'COMPUTER'"
+    // SE 'playerNumber' E' MAGGIORE DI 'pcNumber' ALLORA "winner = 'COMPUTER'"
 if (playerNumber > pcNumber) {
     winner = 'Giocatore';
     // SE 'pcNumber' E' MAGGIORE DI 'playerNumber' ALLORA "winner = 'GIOCATORE'"
@@ -20,16 +23,19 @@ if (playerNumber > pcNumber) {
     winner = 'Computer';
 };
 
-// CREO LA COSTANTE 'msgNumbers' CONTENENTE I NUMERI ESTRATTI PRECEDENTEMENTE
+
+// CREO I MESSAGGI PER LA STAMPA FINALE
+    // CREO LA COSTANTE 'msgNumbers' CONTENENTE I NUMERI ESTRATTI PRECEDENTEMENTE
 let msgNumbers = `Giocatore: ${playerNumber} - Computer: ${pcNumber} -`
 
-// CREO LA COSTANTE 'msg' CON IL MESSAGGIO DEL VINCITORE
+    // CREO LA COSTANTE 'msg' CON IL MESSAGGIO DEL VINCITORE
 const msg = `${msgNumbers} Ha vinto il ${winner}!`;
 
-// CREO LA COSTANTE 'msgPar' CON IL MESSAGGIO DI PARITA'
+    // CREO LA COSTANTE 'msgPar' CON IL MESSAGGIO DI PARITA'
 const msgPar = `${msgNumbers} Risultato di parità. Non ha vinto nessuno!`;
 
-// STAMPO IL RISULTATO IN HTML
+
+// STAMPO IL MESSAGGIO CON IL RISULTATO 
     // SELEZIONO ELEMENTO HTML PER STAMPA
 let result = document.getElementById('result');
 
